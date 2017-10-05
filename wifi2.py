4284a85855f5288c327f7efc5e5a7fbbeca2586c
KOMALJIT SINGH <31124879+komaljit@users.noreply.github.com>
2017-10-05 14:20:10 -0700

    Update wifi2.py

--- wifi2.py
+++ wifi2.py
@@ -1,8 +1,8 @@
 import socket
 import os
 from threading import Thread
-j=1
 # A simple program to check the IP addreses connected to your home or office router.
+
 def check(p,t):
     response=os.popen(p)
     for line in response.readlines():
